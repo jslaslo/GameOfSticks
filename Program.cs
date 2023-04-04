6989faa96469f535208dbbed6859331c593270ab
jslaslo <125125092+jslaslo@users.noreply.github.com>
2023-04-04 14:34:13 +0200

    GameOfSticks
    GameOfSticks

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using F_Delegation.Sticks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace F_Delegation
+{
+    public class Car
+    {
+        int speed = 0;
+        public event Action<int> TooFastDriving;
+        public void Start()
+        {
+            speed = 10;
+            Console.WriteLine("Тртртр.... Завелась");
+        }
+        public void Accelerate()
+        {
+
+            if (speed > 80)
+            {
+                if (TooFastDriving != null)
+                    TooFastDriving(speed);
+            }
+            speed += 10;
+
+        }
+        public void Stop()
+        {
+            speed = 0;
+            Console.WriteLine("ТухТух.... Машина все. Заглохла");
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var game = new GameOfSticks(10, Players.Human);
+            game.MachinePlayed += Game_MachinePlayed;
+            game.HumanPlayed += Game_HumanPlayed;
+            game.EndOfGame += Game_EndOfGame;
+            
+            game.Start();
+
+        }
+        private static void Game_EndOfGame(Players player)
+        {            
+            Console.WriteLine($"Победитель: {player}");
+            Console.WriteLine("Нажмите enter....");
+            Console.ReadLine();
+
+        }
+
+        private static void Game_HumanPlayed(object sender, int[] e)
+        {
+            Console.WriteLine("Возьмите от 1 до 3 палочек");
+
+            bool takenCorrectly = false;
+            while (!takenCorrectly)
+            {
+                if(int.TryParse(Console.ReadLine(), out int takenSticks))
+                {
+                    var game = (GameOfSticks)(sender);
+
+                    try
+                    {
+                        game.HumanTakes(takenSticks);
+                        takenCorrectly = true;
+                    }
+                    catch(ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private static void Game_MachinePlayed(int sticksTaken)
+        {
+            Console.WriteLine($"Машина взяла {sticksTaken}");
+        }
+
+        static void EventsDemo()
+        {
+            Car car = new Car();
+            car.TooFastDriving += HandleOnTooFast;
+
+            car.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                car.Accelerate();
+            }
+            void HandleOnTooFast(int currentSpeed)
+            {
+                Console.WriteLine($"Куда газогнался, машина развалится. Гонишь {currentSpeed}....Глуши мотор ");
+                car.Stop();
+            }
+        }
+
+
+    }
+}
---
+++ Sticks/GameOfSticks.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F_Delegation.Sticks
+{
+    public class GameOfSticks
+    {
+        private readonly Random random;
+        public int InitialSticksNumber { get; }
+        public Players Turn { get; private set; }
+        public int RemaningSticks { get; private set; }
+        public GameStatus GameStatus { get; private set; }
+        public int[] SetSticks { get; private set; }
+
+        public event Action<int> MachinePlayed;
+        public event EventHandler<int[]> HumanPlayed;
+
+        public event Action<Players> EndOfGame;
+        public int OrderOfMove { get; private set; }
+
+        public GameOfSticks(int quantitySticks, Players whoMakesFirstMove)
+        {
+            if (quantitySticks < 7 || quantitySticks > 30)
+            {
+                throw new ArgumentException("Колличество палочек должно быть >= 7 и <=30");
+            }
+            InitialSticksNumber = quantitySticks;
+            SetSticks = new int[InitialSticksNumber];
+            random = new Random();
+            GameStatus = GameStatus.NotStarted;
+            Turn = whoMakesFirstMove;
+        }
+        public void Start()
+        {
+            if (GameStatus == GameStatus.GameIsOver)
+            {
+                SetSticks = new int[InitialSticksNumber];
+            }
+            if (GameStatus == GameStatus.InProgress)
+            {
+                throw new InvalidOperationException("Нельзя вызвать старт...");
+
+            }
+            GameStatus = GameStatus.InProgress;
+            while (GameStatus == GameStatus.InProgress)
+            {
+                GetSetSticks();
+
+                if (Turn == Players.Machine)
+                {
+                    MachineMakeMove();
+                }
+                else
+                {                    
+                    HumanMakeMove();
+                }
+                FireEndOfGameIfRequired();
+
+                Turn = CheckWinner();
+                Console.Clear();
+            }
+        }
+
+        private void FireEndOfGameIfRequired()
+        {
+            if (SetSticks.Length == 0)
+            {
+                GameStatus = GameStatus.GameIsOver;
+                if (EndOfGame != null)
+                {
+                    EndOfGame(CheckWinner());
+                }
+            }
+        }
+
+        private void HumanMakeMove()
+        {
+            if (HumanPlayed != null)
+            {
+                HumanPlayed(this, SetSticks);
+            }
+        }
+
+        private void MachineMakeMove()
+        {
+            Console.WriteLine("Ход компьютера....");
+
+            int maxNumber = SetSticks.Length >= 3? 3: SetSticks.Length;
+            int numberSticks = random.Next(1,maxNumber);
+
+            Console.WriteLine($"Принял решение убрать {numberSticks}");
+            Console.WriteLine("Нажмите enter....");
+            Console.ReadLine();
+
+            TakeSticks(numberSticks);
+        }
+        public void HumanTakes(int numberSticks)
+        {
+            if (numberSticks < 1 || numberSticks > 3)
+            {
+                throw new ArgumentOutOfRangeException("Ты можешь взять от 1 до 3 палочек");
+            }
+
+            if (numberSticks > SetSticks.Length)
+            {
+                throw new ArgumentOutOfRangeException($"Ты можешь взять максимум {SetSticks.Length}");
+            }
+            TakeSticks(numberSticks);
+        }
+
+        private void TakeSticks(int sticks)
+        {
+            int[] newSetSticks = new int[SetSticks.Length - sticks];
+            SetSticks = newSetSticks;
+        }
+
+        public void GetSetSticks()
+        {
+            for (int i = 0; i < SetSticks.Length; i++)
+            {
+                Console.Write("I");
+            }
+            Console.WriteLine();
+        }             
+
+        public Players CheckWinner()
+        {
+            return Turn = Turn == Players.Machine ? Players.Human : Players.Machine;
+        }
+ 
+
+    }
+}
