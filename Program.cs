d585c3ad7db36b7aae40f81fcd55f2bae3c7dafc
jslaslo <125125092+jslaslo@users.noreply.github.com>
2023-04-04 14:36:30 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,32 +9,7 @@ using System.Timers;
 
 namespace F_Delegation
 {
-    public class Car
-    {
-        int speed = 0;
-        public event Action<int> TooFastDriving;
-        public void Start()
-        {
-            speed = 10;
-            Console.WriteLine("Тртртр.... Завелась");
-        }
-        public void Accelerate()
-        {
 
-            if (speed > 80)
-            {
-                if (TooFastDriving != null)
-                    TooFastDriving(speed);
-            }
-            speed += 10;
-
-        }
-        public void Stop()
-        {
-            speed = 0;
-            Console.WriteLine("ТухТух.... Машина все. Заглохла");
-        }
-    }
     internal class Program
     {
 
@@ -44,12 +19,12 @@ namespace F_Delegation
             game.MachinePlayed += Game_MachinePlayed;
             game.HumanPlayed += Game_HumanPlayed;
             game.EndOfGame += Game_EndOfGame;
-            
+
             game.Start();
 
         }
         private static void Game_EndOfGame(Players player)
-        {            
+        {
             Console.WriteLine($"Победитель: {player}");
             Console.WriteLine("Нажмите enter....");
             Console.ReadLine();
@@ -63,7 +38,7 @@ namespace F_Delegation
             bool takenCorrectly = false;
             while (!takenCorrectly)
             {
-                if(int.TryParse(Console.ReadLine(), out int takenSticks))
+                if (int.TryParse(Console.ReadLine(), out int takenSticks))
                 {
                     var game = (GameOfSticks)(sender);
 
@@ -72,36 +47,12 @@ namespace F_Delegation
                         game.HumanTakes(takenSticks);
                         takenCorrectly = true;
                     }
-                    catch(ArgumentException ex)
+                    catch (ArgumentException ex)
                     {
                         Console.WriteLine(ex.Message);
                     }
                 }
             }
         }
-
-        private static void Game_MachinePlayed(int sticksTaken)
-        {
-            Console.WriteLine($"Машина взяла {sticksTaken}");
-        }
-
-        static void EventsDemo()
-        {
-            Car car = new Car();
-            car.TooFastDriving += HandleOnTooFast;
-
-            car.Start();
-            for (int i = 0; i < 10; i++)
-            {
-                car.Accelerate();
-            }
-            void HandleOnTooFast(int currentSpeed)
-            {
-                Console.WriteLine($"Куда газогнался, машина развалится. Гонишь {currentSpeed}....Глуши мотор ");
-                car.Stop();
-            }
-        }
-
-
     }
 }
